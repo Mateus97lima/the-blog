d6ae991687e02f123dc9def1a98e525e9dfb2b04
Mateus lima <matheus2022y@gmail.com>
2025-06-10 20:35:46 -0300

    Adicionei uma imagem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,40 @@
+import { Container } from '@/components/Container';
+import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
+import Image from 'next/image';
+import Link from 'next/link';
 import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div>
-      <header>
-        <h1 className='text-6xl font-bold text-center py-8'>
-          {' '}
-          aqui e um teste
-        </h1>
-      </header>
+    <Container>
+      <Header />
+      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group'>
+        <Link className='w-full h-full overflow-hidden rounded-xl' href='#'>
+          <Image
+          className='group-hover:scale-105 transition'
+            src='/images/bryen_0.png'
+            width={1200}
+            height={720}
+            alt='titulo do post'
+          />
+        </Link>
+        <div>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id tenetur,
+          doloremque fugiat iste, dolor sequi quibusdam illo voluptatem nesciunt
+          nobis rerum omnis, pariatur doloribus corrupti consequuntur deleniti
+          ut? Earum, expedita.
+        </div>
+      </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
 
       <footer>
-        <p className='text-6xl font-bold text-center py-8'> aqui e um footer</p>
+        <p className='text-6xl font-bold text-center py-8'> footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import clsx from "clsx"
+import Link from "next/link"
+
+export function Header(){
+    return(
+        //aprendendo resposividade: sm,md,lg,xl//
+        <h1 className={clsx('text-4xl/normal font-extrabold py-8',
+        'sm:text-5xl/normal sm:py-10',
+        'md:text-6xl/normal py-12',
+        'lg:text-7xl/normal py-15')}>
+            <Link href="#">The Blog</Link>
+        </h1>
+    )
+}
